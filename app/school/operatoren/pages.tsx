1cd23b60987c233486f159a17d358802311fd6a0
DerXamtes <ansorg.maximilian@gmail.com>
2023-03-23 10:31:26 +0100

    idk

---
+++ app/school/operatoren/pages.tsx
@@ -0,0 +1,12 @@
+export default function operatoren() {
+  return (
+    <html lang="en">
+      <body>
+        <main>
+          <h1>School projects</h1>
+          <p>Error 404 Content not found</p>
+        </main>
+      </body>
+    </html>
+  )
+}
